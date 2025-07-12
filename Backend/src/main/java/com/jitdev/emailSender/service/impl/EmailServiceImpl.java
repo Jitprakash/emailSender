2507a0b48fa7ca9c93d1b51cca1bc8b30359b6ee
Jitprakash <jitprakash2020@gmail.com>
2025-07-12 19:05:33 +0530

    other mail sending services like,sending to multiple people,sending HTMl content & sending file attachment

--- Backend/src/main/java/com/jitdev/emailSender/service/impl/EmailServiceImpl.java
+++ Backend/src/main/java/com/jitdev/emailSender/service/impl/EmailServiceImpl.java
@@ -1,12 +1,16 @@
 package com.jitdev.emailSender.service.impl;
 
 import com.jitdev.emailSender.service.EmailService;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
@@ -22,7 +26,7 @@ public class EmailServiceImpl implements EmailService {
 
     //get the user mail
     @Value("${my-mail}")
-    String userName;
+    String userMail;
 
     @Autowired
     public EmailServiceImpl(JavaMailSender mailSender) {
@@ -37,7 +41,7 @@ public class EmailServiceImpl implements EmailService {
         simpleMailMessage.setTo(to);//Receiver mail address
         simpleMailMessage.setSubject(sub);//subject of the mail
         simpleMailMessage.setText(message);//Text message
-        simpleMailMessage.setFrom(userName);//sender mail
+        simpleMailMessage.setFrom(userMail);//sender mail
         //Send the mail
         mailSender.send(simpleMailMessage);//it takes a simpleMailMessage or MimeMessage as input
 
@@ -48,16 +52,71 @@ public class EmailServiceImpl implements EmailService {
 
     @Override
     public void sendEmail(String[] to, String sub, String message) {
+        //Create a simple Mail Message
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        //Set Mail properties
+        simpleMailMessage.setTo(to);
+        simpleMailMessage.setFrom(userMail);
+        simpleMailMessage.setSubject(sub);
+        simpleMailMessage.setText(message);
+
+        //send the mail
+        mailSender.send(simpleMailMessage);
+
+        //log info
+        logger.info("Email Sent successfully");
 
     }
 
     @Override
     public void sendEmailWithHtml(String to, String sub, String htmlContent) {
+        //Create a Mime Message
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
 
+        try {
+            //Create a Mime message helper
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
+            //set message data
+            messageHelper.setTo(to);
+            messageHelper.setFrom(userMail);
+            messageHelper.setSubject(sub);
+            messageHelper.setText(htmlContent,true);
+
+            //send mail
+            mailSender.send(mimeMessage);
+
+            //log info
+            logger.info("Email Sent!");
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void sendEmailWithFile(String to, String sub, String message, File file) {
+        //Create a Mime Message
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
 
+        try {
+            //Create a Mime message helper
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage, true);
+            //set message data
+            messageHelper.setTo(to);
+            messageHelper.setFrom(userMail);
+            messageHelper.setSubject(sub);
+            messageHelper.setText(message);
+
+            //Create a File System Resource
+            FileSystemResource fileSystemResource = new FileSystemResource(file);
+            messageHelper.addAttachment(fileSystemResource.getFilename(),file );
+
+            //send mail
+            mailSender.send(mimeMessage);
+
+            //log info
+            logger.info("Email Sent");
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
